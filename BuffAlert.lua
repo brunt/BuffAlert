4ecbaced5b655faf883867f9dfaf35eb7324830f
brunt <bryantdeters@gmail.com>
2024-02-20 10:14:00 -0600

    simplify

--- BuffAlert.lua
+++ BuffAlert.lua
@@ -1,16 +1,8 @@
 local events = CreateFrame("Frame", nil, UIParent)
-events:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_SELF_BUFFS")
 events:RegisterEvent("CHAT_MSG_SPELL_SELF_BUFF")
-events:RegisterEvent("CHAT_MSG_SPELL_SELF_DAMAGE")
-
-local messages = {
-    "You gain Not There.",
-}
 
 events:SetScript("OnEvent", function()
-    for _, msg in pairs(messages) do
-        if (string.find(arg1, msg)) then
-            PlaySoundFile[[Interface\AddOns\BuffAlert\sound.ogg]]
-        end
+    if (string.find(arg1, "You gain Not There.")) then
+        PlaySoundFile[[Interface\AddOns\BuffAlert\sound.ogg]]
     end
 end)
